0412401a99c84c02f7a7cc1e884cee67cb999ef6
Jordan <jordanhernando1085@gmail.com>
2021-01-30 19:48:59 -0300

    creando formulario crear cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -6,6 +6,39 @@ export default function CrearCuenta() {
     <Fragment>
       <Layout>
         <h1>Crear Cuenta</h1>
+        <form>
+          <div>
+            <labe htmlFor="nombre">Nombre</labe>
+            <input 
+              type="text"
+              id="nombre"
+              placeholder="Tu Nombre"
+              name="nombre"
+            />
+          </div>
+          <div>
+            <labe htmlFor="email">Email</labe>
+            <input 
+              type="email"
+              id="email"
+              placeholder="Tu Email"
+              name="email"
+            />
+          </div>
+          <div>
+            <labe htmlFor="password">Password</labe>
+            <input 
+              type="password"
+              id="password"
+              placeholder="Tu Password"
+              name="password"
+            />
+          </div>
+          <input 
+            type="submit"
+            value="Crear Cuenta"
+          />
+        </form>
       </Layout>
     </Fragment>
   )
