6e70bc416b54c133fafa246dff00a06270af0e69
Jordan <jordanhernando1085@gmail.com>
2021-01-30 16:33:26 -0300

    Creando la navegacion

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react';
-import Link from 'next/link';
 import Header from './Header';
 
 const Layout = props => {
     return (
         <Fragment>
             <Header />
-            
+
             <main>
 
                 {props.children}
