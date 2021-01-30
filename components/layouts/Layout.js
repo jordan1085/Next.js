90b054ff364bf78ed249dc2a8282b5127d86923e
Jordan <jordanhernando1085@gmail.com>
2021-01-30 16:52:31 -0300

    Cargando google fonts y archivos css img static

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import Header from './Header';
 import { Global, css } from '@emotion/react';
+import Head from 'next/head';
+
 
 const Layout = props => {
     return (
@@ -49,6 +51,14 @@ const Layout = props => {
                     }
                 `}
             />
+
+            <Head>
+                <title>Product Hunt Firebase y Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossOrigin="anonymous" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,700;1,400&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
+                <link href="/static/css/app.css" rel="stylesheet" />
+            </Head>
 
             <Header />
 
