d93a3dda8819e366552b993977ad71612c18356b
Jordan <jordanhernando1085@gmail.com>
2021-01-30 15:52:54 -0300

    agregando elementos al header

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react';
 import Link from 'next/link';
+import Header from './Header';
 
 const Layout = props => {
     return (
         <Fragment>
-            <h1>Header</h1>
-            <na>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">Nosotros</Link>
-            </na>
+            <Header />
+            
             <main>
 
                 {props.children}
