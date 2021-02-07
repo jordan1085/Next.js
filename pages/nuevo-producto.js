20f0e95cba0235a2739a1cdbba7fe50003014456
Jordan <jordanhernando1085@gmail.com>
2021-02-07 00:03:20 -0300

    Habilitando firestorage y subiendo imagen

--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { css } from '@emotion/react';
 import Router, { useRouter } from 'next/router';
+import FileUploader from 'react-firebase-file-uploader';
 import Layout from '../components/layouts/Layout';
 import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
 
@@ -20,7 +21,13 @@ const STATE_INICIAL = {
 
 export default function NuevoProducto() {
 
-  const [ error, guardarError] = useState(false);
+  // state image
+  const [nombreImagen, guardarNombre] = useState('');
+  const [subiendo, guardarSubiendo] = useState(false);
+  const [progreso, guardarProgreso] = useState(0);
+  const [urlimagen, guardarUrlImagen] = useState('');
+
+  const [error, guardarError] = useState(false);
 
   const { valores, errores, handleSubmit, handleChange, handleBlur } = useValidacion(STATE_INICIAL, validarCrearProducto, crearProducto);
 
@@ -34,7 +41,7 @@ export default function NuevoProducto() {
 
   async function crearProducto() {
     // Si el usuario no esta autenticado llevar al login
-    if(!usuario) {
+    if (!usuario) {
       return router.push('/login');
     }
 
@@ -43,6 +50,7 @@ export default function NuevoProducto() {
       nombre,
       empresa,
       url,
+      urlimagen,
       descripcion,
       votos: 0,
       comentarios: [],
@@ -51,7 +59,37 @@ export default function NuevoProducto() {
 
     // Insertar en la BD
     await firebase.db.collection('productos').add(producto);
+
+    return router.push('/');
   }
+
+
+  const handleUploadStart = () => {
+    guardarProgreso(0);
+    guardarSubiendo(true);
+  }
+
+  const handleProgress = progreso => guardarProgreso({ progreso });
+
+  const handleUploadError = error => {
+    guardarSubiendo(error);
+    console.error(error);
+  };
+
+  const handleUploadSuccess = nombre => {
+    guardarProgreso(100);
+    guardarSubiendo(false);
+    guardarNombre(nombre)
+    firebase
+      .storage
+      .ref("productos")
+      .child(nombre)
+      .getDownloadURL()
+      .then(url => {
+        console.log(url);
+        guardarUrlImagen(url);
+      });
+  };
 
 
   return (
@@ -73,90 +111,91 @@ export default function NuevoProducto() {
               <legend>Información General</legend>
 
               <Campo>
-                  <label htmlFor="nombre">Nombre</label>
-                  <input 
-                      type="text"
-                      id="nombre"
-                      placeholder="Tu Nombre"
-                      name="nombre"
-                      value={nombre}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                  />
+                <label htmlFor="nombre">Nombre</label>
+                <input
+                  type="text"
+                  id="nombre"
+                  placeholder="Tu Nombre"
+                  name="nombre"
+                  value={nombre}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
               </Campo>
 
-              {errores.nombre && <Error>{errores.nombre}</Error> }
-  
-              <Campo>
-                  <label htmlFor="empresa">Empresa</label>
-                  <input 
-                      type="text"
-                      id="empresa"
-                      placeholder="Tu Empresa o Compañia"
-                      name="empresa"
-                      value={empresa}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                  />
-              </Campo>
-
-              {errores.empresa && <Error>{errores.empresa}</Error> }
-{/* 
-              <Campo>
-                  <label htmlFor="empresa">Imagen</label>
-                  <input 
-                      type="file"
-                      id="imagen"
-                      name="imagen"
-                      value={imagen}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                  />
-              </Campo>
-
-              {errores.imagen && <Error>{errores.imagen}</Error> } */}
+              {errores.nombre && <Error>{errores.nombre}</Error>}
 
               <Campo>
-                  <label htmlFor="empresa">URL</label>
-                  <input 
-                      type="url"
-                      id="url"
-                      name="url"
-                      placeholder="URL de tu producto"
-                      value={url}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                  />
+                <label htmlFor="empresa">Empresa</label>
+                <input
+                  type="text"
+                  id="empresa"
+                  placeholder="Tu Empresa o Compañia"
+                  name="empresa"
+                  value={empresa}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
               </Campo>
 
-              {errores.url && <Error>{errores.url}</Error> }
+              {errores.empresa && <Error>{errores.empresa}</Error>}
+
+              <Campo>
+                <label htmlFor="imagen">Imagen</label>
+                <FileUploader
+                  accept="image/*"
+                  id="imagen"
+                  name="imagen"
+                  randomizeFilename
+                  storageRef={firebase.storage.ref("productos")}
+                  onUploadStart={handleUploadStart}
+                  onUploadError={handleUploadError}
+                  onUploadSuccess={handleUploadSuccess}
+                  onProgress={handleProgress}
+                />
+              </Campo>
+
+              <Campo>
+                <label htmlFor="empresa">URL</label>
+                <input
+                  type="url"
+                  id="url"
+                  name="url"
+                  placeholder="URL de tu producto"
+                  value={url}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+
+              {errores.url && <Error>{errores.url}</Error>}
 
             </fieldset>
-            
+
             <fieldset>
               <legend>Sobre Tu Producto</legend>
-              
+
               <Campo>
-                  <label htmlFor="descripcion">Descripcion</label>
-                  <textarea 
-                      id="descripcion"
-                      name="descripcion"
-                      value={descripcion}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                  />
+                <label htmlFor="descripcion">Descripcion</label>
+                <textarea
+                  id="descripcion"
+                  name="descripcion"
+                  value={descripcion}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
               </Campo>
 
-              {errores.descripcion && <Error>{errores.descripcion}</Error> }
+              {errores.descripcion && <Error>{errores.descripcion}</Error>}
 
             </fieldset>
 
-              {error && <Error>{error} </Error>}
-  
-              <InputSubmit 
-                type="submit"
-                value="Crear Producto"
-              />
+            {error && <Error>{error} </Error>}
+
+            <InputSubmit
+              type="submit"
+              value="Crear Producto"
+            />
           </Formulario>
         </>
       </Layout>
